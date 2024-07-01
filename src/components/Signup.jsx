4c176b71738a6b919e6d5183d2802e0dd88b3bf2
RiteshDhiman <riteshdhiman24@gmail.com>
2024-07-01 14:37:52 +0530

    First Commit

---
+++ src/components/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+      asdkfjasl
+    </div>
+  )
+}
+
+export default Signup
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Signup from '../../components/Signup'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
