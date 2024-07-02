0cd2965c776186abadb1a9ae33807f40e70fc48e
Garima Jaiswal <122000586+GarimaJaiswal12@users.noreply.github.com>
2024-07-02 06:41:29 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,17 +4,17 @@ function Home() {
   return (
     <div className="h-screen flex justify-center items-center bg-gray-900 text-white">
       <div className="max-w-md mx-auto text-center">
-        <h1 className="text-3xl font-bold mb-4">
-          <span className="text-white">Best</span> 
-          <span className="text-green-500" style={{ color: 'rgba(102, 169, 71, 1)' }}>Resume</span>
-        </h1>
+      <h1 className="text-3xl font-bold mb-4">
+  <span className="text-white mr-1">Best</span> 
+  <span className="text-green-500" style={{ color: 'rgba(102, 169, 71, 1)' }}>Resume</span>
+   </h1>
         <h2 className="text-2xl font-semibold mb-8">Everything</h2>
         <p className="text-lg mb-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         <div className="flex justify-center gap-4">
           <button className="border border-green-500 hover:bg-green-700 text-white-500 hover:text-white font-bold py-2 px-4 rounded">
             Check your score
           </button>
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+          <button  style={{ backgroundColor: 'rgba(102, 169, 71, 1)' }} className=" hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
             Build your resume
           </button>
         </div>
