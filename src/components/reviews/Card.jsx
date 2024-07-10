1751a9ef15539a19f8cb612a7718838c58f2ef9a
Shreya Mathur <smathur245@gmail.com>
2024-07-10 20:12:25 +0530

    fixed reviews

--- src/components/reviews/Card.jsx
+++ src/components/reviews/Card.jsx
@@ -5,19 +5,20 @@ import Stars from '/images/reviews/stars.png'
 function Card(props) {
 
     const profile_icon = props.image;
+    const stars = props.stars;
 
 
   return (
     <div className='bg-[#66A947] font-manrope w-full h-60 rounded-xl flex justify-between items-center overflow-hidden'>
-                <div className='profile-icon relative w-1/4 h-full'>
+                <div className='profile-icon relative w-1/3 h-full'>
                     <div className=' left-0 bottom-0 absolute w-full'><img src={Frame} /></div>
-                    <div className=' left-3 bottom-1 absolute w-44'><img src={profile_icon} /></div>
+                    <div className=' left-3 bottom-4 absolute w-48'><img src={profile_icon} /></div>
                 </div>
 
-                <div className="text text-white p-10 w-3/4 h-full flex-col justify-between items-center">
+                <div className="text text-white p-10 w-2/3 h-full flex-col justify-between items-center">
                     <div className='flex gap-10 items-center mb-4'>
                         <div className='font-bold text-xl'>{props.title}</div>
-                        <div className=' w-2/12'><img src={Stars} /></div>
+                        <div className=' w-2/12'><img src={props.stars} /></div>
                     </div>
                     <div className=' leading-6 mb-5 text-lg'>{props.desc}</div>
                     <div className='font-semibold text-md'>{props.name}</div>
