177b361ec37cc9dc030a8e41992e4faaf3d70d65
RiteshDhiman <riteshdhiman24@gmail.com>
2024-07-11 11:20:08 +0530

    Check Resume Updated

--- src/components/checkscore/Checkscore.jsx
+++ src/components/checkscore/Checkscore.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 
 function CheckScore() {
@@ -13,115 +15,107 @@ function CheckScore() {
   };
 
   const handleCheckScore = () => {
-    
     console.log('Checking score for:', selectedFile, jobDescription);
   };
 
   const handleUpload = () => {
- 
     console.log('Selected file:', selectedFile);
   };
 
+  // eslint-disable-next-line no-unused-vars
   const wordCount = jobDescription.trim().split(/\s+/).length;
 
   return (
     <div className="bg-gray-900 text-white min-h-screen flex flex-col items-center">
-      <h1 className="w-[35.3125rem] text-white text-center mt-14 font-bold mt-6 text-3xl mx-auto font-krona break-words mt-20">
+      <h1 className="w-[35.3125rem] text-white text-center font-bold mt-6 text-3xl mx-auto font-Krona One break-words mt-20">
         Check Resume
       </h1>
-     <div className='left-8 absolute mt-14'>
-      <div className="bg-gray-900 rounded-lg p-6 shadow-md w-104 mt-8">
-        <div className="flex items-center mb-4">
-          <img src="/images/uploadResume.png" alt="Upload Icon" className="h-14 w-14 text-green-500 mr-2" />
-          <p className="text-[2.09rem] text-[#66A947] font-Inter font-medium break-words">
-            <span>Upload Resume</span>
-          </p>
-        </div>
-        <div className="w-[34.3125rem] text-[#A9ACB4] text-[1.784rem] font-Inter font-medium break-words">
-          Select and upload the file of your choice
-        </div>
-        <p className="text-gray-400 mb-4">
-          <br></br>
-          ___________________________________________________________________________________
-        </p>
 
-        <div className="bg-gray-900 rounded-lg p-4 border-dashed border-2 border-gray-500">
-          <div className="flex items-center justify-center">
-            <img src="/images/danalitics.png" alt="Danalitic Logo" className="h-24" />
+      <div className="flex justify-center mt-8 w-full max-w-6xl">
+        <div className="bg-gray-900 rounded-lg p-6 shadow-md w-[calc(50% - 1rem)] mx-4 mb-8 h-[34rem]">
+          <div className="flex items-center mb-4">
+            <img src="./images/uploadResume.png" alt="Upload Icon" className="h-11 w-12 text-green-500 mr-2" />
+            <p className="text-[2.09rem] text-[#66A947] font-Inter font-medium break-words">
+              <span>Upload Resume</span>
+            </p>
           </div>
-          <p className="text-center text-[#66A947] text-[1.845rem] font-Inter font-medium break-words">
-            Choose a file or drag & drop it here
-          </p>
-          
-          <p className="text-center text-[#A9ACB4] text-[1.599rem] font-Inter font-medium break-words">
-            PDF and DOC formats, up to 50MB
-          </p>
-          <input type="file" className="hidden" id="resumeUpload" onChange={handleFileChange} />
-          <label
-            htmlFor="resumeUpload"
-            className="w-[14.2075rem] mt-6 mb-6 ml-36 h-[4.1825rem] px-[2.029375rem] py-[0.984375rem] bg-white rounded-2xl border-2 border-slate-300 flex justify-center items-center gap-[0.615rem] text-center text-[#54575C] text-[1.845rem] font-Inter font-medium break-words cursor-pointer"
-          >
-            Browse File
-          </label>
-        </div>
-        <div className="flex justify-between mt-6">
-         
-          <p className="text-gray-400 mt-4 text-[1.784rem] mr-3 font-medium font-Inter">Don't have a resume?</p>
-        
-          <button
-            className="relative w-[17.904375rem] h-[4.4375rem] bg-[#66A947] text-white font-bold rounded-2xl border border-[#66A947] cursor-pointer"
-            onClick={handleUpload}
-          >
-            <button className="w-[14.52375rem] absolute left-[1.69rem] top-[1rem] text-[1.61375rem] font-Poppins font-normal break-words">
-              Build your resume
+          <div className="w-[34.3125rem] text-[#A9ACB4] text-[1.784rem] font-Inter font-medium break-words">
+            Select and upload the file of your choice
+          </div>
+          <div className="flex justify-between mt-6">
+            <p className="text-gray-400 text-2xl mt-4  font-medium font-Inter">Don't have a resume?</p>
+            <button
+              className="relative w-72  h-14 bg-[#66A947] text-white font-bold rounded-xl border border-[#66A947] cursor-pointer"
+              onClick={handleUpload}
+            >
+              <span className="w-full mt-2absolute left-0 top-2 text-2xl font-[Poppins] font-normal break-words">
+                Build your resume
+              </span>
             </button>
-          </button>
-          <br></br>
-         
-        </div>
-        
-      </div>
-      
-      </div>
-  
-      <div className=' relative   mt-96 p-4 ml-80 '>
-
-      <div className="flex items-center mt-80 ml-96 ">
-      
-      <h2 className="text-lime-600 text-[2.09125rem] font-medium font-Inter mb-4 ml-32 text-center">Enter Job Description</h2>
-     
-      </div>
-      
-     
-      <div className="bg-gray-900 rounded-lg p-4 border-dashed border-2 border-gray-500  ml-96 ">
-        <div className="w-[33.75rem] h-80 relative">
-          
-          <div className="flex flex-col items-center justify-center h-full">
-        
-            <textarea
-              className="w-full h-full p-0 bg-gray-900 text-center text-white resize-none outline-none"
-              placeholder="Enter Text"
-              value={jobDescription}
-              onChange={handleChange}
-            />
-            <img src="/images/cloud.png" alt="cloud Logo" className="h-24 mt-2" />
-            <p className="text-gray-400 mt-2">{wordCount} / 1000 words</p>
+          </div>
+          <p className="text-gray-400 mb-4">
+            <br />
+            _______________________________________________________________________________
+          </p>
+          <div className="bg-gray-900 rounded-lg p-4 border-dashed border-2 border-gray-500 h-[20rem]">
+            <div className="flex items-center justify-center">
+              <img src="./images/upload.png" alt="Danalitic Logo" className="h-11 w-12" />
+            </div>
+            <br></br>
+            <p className="text-center text-[#66A947] text-[1.845rem] font-[Inter] font-medium break-words">
+              Choose a file or drag & drop it here
+            </p>
+            <p className="text-center text-[#A9ACB4] text-[1.599rem] font-[Inter] font-medium break-words">
+              PDF and DOC formats, up to 50MB
+            </p>
+            <input type="file" className="hidden" id="resumeUpload" onChange={handleFileChange} />
+            <label
+              htmlFor="resumeUpload"
+              className="w-[14.2075rem] mt-6 mb-6 ml-36 h-[4.1825rem] px-[2.029375rem] py-[0.984375rem] bg-white rounded-2xl border-2 border-slate-300 flex justify-center items-center gap-[0.615rem] text-center text-[#54575C] text-[1.845rem] font-[Inter] font-medium break-words cursor-pointer"
+            >
+              Browse File
+            </label>
           </div>
         </div>
-      </div>
-      </div>
-     
-      <button
-        className="relative w-[26.686875rem] h-[4rem] mt-20 mb-8 bg-lime-600 rounded-[1.86625rem] border border-black text-white text-[2.409375rem] font-normal font-Poppins flex items-center justify-center"
-        onClick={handleCheckScore}
-      >
-        Check Score
-      </button>
+        
 
-   </div>
+    
+        <div className="bg-gray-900 rounded-lg p-6 shadow-md w-[calc(50% - 1rem)] mx-4 mb-8 h-[34rem]">
+          <div className="flex items-center mb-4 ">
+            <img src="./images/uploadResume.png" alt="cloud Logo" className="w-12 h-11  mr-2" />
+            <p className="text-[2.09rem] text-[#66A947] font-Inter font-medium break-words">
+              <span>Enter Job Description</span>
+            </p>
+          </div>
+          
+          <div className="w-[34.3125rem] text-[#A9ACB4] text-[1.784rem] font-Inter font-medium break-words">
+          Please enter job description details
+          </div>
+         
+          <textarea
+            className="w-full h-[20rem] mt-36 p-4 rounded-lg bg-gray-900 text-center text-white resize-none outline-none border-dashed border-2 border-gray-500"
+            placeholder="Enter Text"
+            value={jobDescription}
+            onChange={handleChange}
+          />
+       
+        </div>
+      </div>
       
-     
+
+    
+      <div className="mt-12 mb-8">
+        <button
+          className="relative w-[26.686875rem] h-[4rem] bg-[#66A947] rounded-[1.86625rem] border border-black text-white text-[2.409375rem] font-normal font-Poppins flex items-center justify-center"
+          onClick={handleCheckScore}
+        >
+          Check Score
+        </button>
+      </div>
+      </div>
+ 
+   
   );
 }
 
-export default CheckScore;
+export default CheckScore;
