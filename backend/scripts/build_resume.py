b793f5148b3e26319f939d98b230416ddd0bb389
saransh-2021 <saranshshukla2003@gmail.com>
2024-08-17 11:58:44 +0530

    chore: Refactor build_resume function to use flatten_dict for data preparation

--- backend/scripts/build_resume.py
+++ backend/scripts/build_resume.py
@@ -1,6 +1,7 @@
 from scripts.utils import to_json_formatted
 from scripts.communicate_with_gpt import build_resume_with_gpt
-from flatten_dict.reducers import make_reducer, flatten
+from flatten_dict.reducers import make_reducer
+from flatten_dict import flatten
 
 def build_resume(json_request_data):
     data_prep = flatten(json_request_data, reducer=make_reducer(delimiter='_'), enumerate_types=(list,), keep_empty_types=(dict,))
