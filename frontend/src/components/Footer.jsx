b15335e78e267024d339e676f74fe51c0e942f80
Arjun Bhardwaj <90506928+master-coco@users.noreply.github.com>
2024-08-30 14:41:19 +0530

    changed footer image

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <img src="/images/signup/google.png" className="h-8" alt="Flowbite Logo" />
+                        <img src="/images/resume__logo.png" className="h-12 " />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ResumePulse</span>
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
